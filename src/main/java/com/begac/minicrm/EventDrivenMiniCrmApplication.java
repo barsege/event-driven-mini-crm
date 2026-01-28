9d11cdffcd2f5df10cfb635c482eee662e191312
barsege <barisegeacar@gmail.com>
2026-01-28 18:28:07 +0300

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/begac/minicrm/EventDrivenMiniCrmApplication.java
@@ -0,0 +1,13 @@
+package com.begac.minicrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenMiniCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenMiniCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/begac/minicrm/EventDrivenMiniCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begac.minicrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenMiniCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
